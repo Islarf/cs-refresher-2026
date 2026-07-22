c83609e60378e96d75ba7670571ae592153db12b
Darren <db.darren6546@gmail.com>
2026-07-22 11:14:11 +0100

    Switch looping, and product clarification
    Clarified the product to be added is correct. And allowed the menu selection to loop on the same until chosen to leave.

--- Week-01-Basics/CSRefresherProject/Program.cs
+++ Week-01-Basics/CSRefresherProject/Program.cs
@@ -44,26 +44,37 @@ void RunMenu()
     Console.WriteLine("4: Exit the program");
 
     String MenuChoice = Console.ReadLine() ?? "0";
-    switch(MenuChoice)
-    {
-        case "1":
-            Console.WriteLine("You have chosen to search for a product.");
-            break;
-        case "2":
-            Console.WriteLine("You have chosen to add a product to the tracker.");
-            ProductManager newProduct = new ProductManager();
-            newProduct.AddProduct_Console();
-            break;
-        case "3":
-            Console.WriteLine("You have chosen to remove a product from the tracker.");
-            break;
-        case "4":
-            Console.WriteLine("You have chosen to exit the program.");
-            isRunning = false;
-            break;
-        default:
-            Console.WriteLine("Invalid choice. Please try again.");
-            break;
+    while (MenuChoice != "-1") {
+        switch (MenuChoice)
+        {
+            case "1":
+                Console.WriteLine("You have chosen to search for a product.");
+                MenuChoice = "-1";
+                break;
+            case "2":
+                Console.WriteLine("You have chosen to add a product to the tracker.");
+                ProductManager newProduct = new ProductManager();
+                newProduct.AddProduct_Console();
+                Console.WriteLine("Would you like to add another product? (Y/N)");
+                if (Console.ReadLine()?.ToUpper() != "Y")
+                {
+                    MenuChoice = "-1";
+                }
+
+                break;
+            case "3":
+                Console.WriteLine("You have chosen to remove a product from the tracker.");
+                MenuChoice = "-1";
+                break;
+            case "4":
+                Console.WriteLine("You have chosen to exit the program.");
+                MenuChoice = "-1";
+                isRunning = false;
+                break;
+            default:
+                Console.WriteLine("Invalid choice. Please try again.");
+                break;
+        }
     }
 
 }
@@ -92,8 +103,8 @@ public class ProductManager
         Console.Write("Enter Unit Cost: ");
         decimal.TryParse(Console.ReadLine(), out decimal o_cost);
         Console.Write("Enter Quantity: ");
-        int.TryParse(Console.ReadLine(),out int o_quantity);
-        
+        int.TryParse(Console.ReadLine(), out int o_quantity);
+
         Product newProduct = new Product
         {
             ItemID = id,
@@ -102,7 +113,17 @@ public class ProductManager
             Quantity = o_quantity
         };
         _products.Add(newProduct);
-        Console.WriteLine($"Added {newProduct.ItemDesc} successfully");
+        Console.WriteLine($"You created the product: {newProduct.ItemID} : {newProduct.ItemDesc}, Quantity: {newProduct.Quantity}, Total Cost: {newProduct.TotalCost}");
+        Console.WriteLine("Confirm that you would like to add this Product to the tracker? (Y/N)");
+        if (Console.ReadLine()?.ToUpper() == "Y")
+        {
+            Console.WriteLine("Product added to the tracker.");
+        }
+        else
+        {
+            _products.Remove(newProduct);
+            Console.WriteLine("Product not added to the tracker.");
+        }
     }
 
 }
