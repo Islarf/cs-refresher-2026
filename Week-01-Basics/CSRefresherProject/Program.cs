7aedbf507147b5f346b224197365acd1054b8582
Darren <db.darren6546@gmail.com>
2026-07-22 09:21:13 +0100

    comment removal
    removed the comment of the old code, this is saved on githubs' history

--- Week-01-Basics/CSRefresherProject/Program.cs
+++ Week-01-Basics/CSRefresherProject/Program.cs
@@ -65,49 +65,6 @@ void RunMenu() {
             break;
     }
 
-
-    /*
-    //Acquiring product information from the user
-    Console.WriteLine("\n\n\n\n");
-    Console.WriteLine("What is the Item ID?: ");
-    string itemID = Console.ReadLine() ?? "0";
-
-    Console.WriteLine("What is the ItemDescription?: ");
-    string itemDesc = Console.ReadLine() ?? "N/A";
-
-    Console.WriteLine("What is the Unit Cost? (€): ");
-    decimal.TryParse(Console.ReadLine(), out decimal unitCost);
-
-    //no if used. the TryParse outputs a 0 if empty anyway.
-    Console.WriteLine("What is the quantity purchased?: ");
-    int.TryParse(Console.ReadLine(), out int quantityPurchased);
-
-    //Confirming the details entered are correct
-    Console.WriteLine("========================================");
-    Console.WriteLine("\n\n\n**" + userName + " is adding the following product to the tracker: **");
-    Console.WriteLine($"Item ID: {itemID}");
-    Console.WriteLine($"Item Description: {itemDesc}");
-    Console.WriteLine($"Unit Cost : {unitCost:C2}");
-    Console.WriteLine($"Quantity Purchased: {quantityPurchased}");
-    Console.WriteLine($"Total Cost: {(unitCost * quantityPurchased):C2}");
-    Console.WriteLine("========================================");
-
-    //Acquiring confirmation to push the product to the tracker, and showing an appropriate message
-    Console.WriteLine("\n\n\n");
-    Console.WriteLine("Do you want to publish this product to the tracker? (Y/N): ");
-    if (Console.ReadLine()?.ToUpper() == "Y") {
-        Console.WriteLine("Product published to the tracker!");
-    }
-    else    {
-        Console.WriteLine("Product not published to the tracker.");
-    }
-    
-
-    //Loop clause; asking the user if they want to try again or enter another product
-    if (Console.ReadLine()?.ToUpper() == "N")    {
-        isRunning = false;
-    }
-    */
 }
 
 Console.WriteLine("\nThank you for using Expense Tracker. Goodbye!");
