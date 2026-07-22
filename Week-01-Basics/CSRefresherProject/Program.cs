3c4b3dc2596ccd01ff986640386209a566c585b8
Darren <db.darren6546@gmail.com>
2026-07-22 09:11:18 +0100

    Json File and Menu
    Updated the project to an Inventory and Expense tracker. Added a Json file with an example item. and updated the main program to have a menu.

--- Week-01-Basics/CSRefresherProject/Program.cs
+++ Week-01-Basics/CSRefresherProject/Program.cs
@@ -1,4 +1,4 @@
-﻿// Week 1 Project :: Expense and Currency Tracker
+﻿// Week 1 Project :: Inventory and Expense Tracker
 
 //so the print later can output the euro symbol
 Console.OutputEncoding = System.Text.Encoding.UTF8;
@@ -10,7 +10,7 @@ bool isRunning = true;
 
 //Introduction
 Console.WriteLine("==================================");
-Console.WriteLine("== Expense and Currency Tracker ==");
+Console.WriteLine("== Inventory and Expense Tracker ==");
 Console.WriteLine("==================================");
 Console.WriteLine("\n\n\n\n");
 
@@ -24,6 +24,41 @@ Console.WriteLine($"Hello, {userName}!");
 
 
 while (isRunning)   {
+
+    //MainMenu
+    Console.WriteLine("What would you like to do?");
+    Console.WriteLine("1: Search for a product");
+    Console.WriteLine("2: Add a product to the tracker");
+    Console.WriteLine("3: Search for multiple products");
+    Console.WriteLine("4: Remove a product from the tracker");
+    Console.WriteLine("5: Exit the program");
+
+    String MenuChoice = Console.ReadLine() ?? "0";
+    switch(MenuChoice)
+    {
+        case "1":
+            Console.WriteLine("You have chosen to search for a product.");
+            break;
+        case "2":
+            Console.WriteLine("You have chosen to add a product to the tracker.");
+            break;
+        case "3":
+            Console.WriteLine("You have chosen to search for multiple products.");
+            break;
+        case "4":
+            Console.WriteLine("You have chosen to remove a product from the tracker.");
+            break;
+        case "5":
+            Console.WriteLine("You have chosen to exit the program.");
+            isRunning = false;
+            break;
+        default:
+            Console.WriteLine("Invalid choice. Please try again.");
+            break;
+    }
+
+
+    /*
     //Acquiring product information from the user
     Console.WriteLine("\n\n\n\n");
     Console.WriteLine("What is the Item ID?: ");
@@ -58,12 +93,13 @@ while (isRunning)   {
     else    {
         Console.WriteLine("Product not published to the tracker.");
     }
+    
+
     //Loop clause; asking the user if they want to try again or enter another product
-    Console.WriteLine("Do you want to try again or enter another product? (Y/N): ");
     if (Console.ReadLine()?.ToUpper() == "N")    {
         isRunning = false;
     }
-
+    */
 }
 
 Console.WriteLine("\nThank you for using Expense Tracker. Goodbye!");
